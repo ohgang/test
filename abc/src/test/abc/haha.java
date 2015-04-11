df5fc002ded40ee46fe9cde162525bc6ca21ae29
Administrator <Administrator@MSDN-SPECIAL>
2015-04-12 05:09:11 +0900

    연습입니다

---
+++ abc/src/test/abc/haha.java
@@ -0,0 +1,7 @@
+package test.abc;
+
+public class haha {
+	public static void main(String args[]){
+		System.out.println("hi?");
+	}
+}
--- test/src/com/test/testsrc.java
+++
@@ -1,7 +0,0 @@
-package com.test;
-
-public class testsrc {
-	public static void main(String args[]){
-		System.out.println("java test");
-	}
-}
